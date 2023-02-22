adf01f2d2dd807b1bd3767f7586f7d3669af6994
redflashjesse <105519994+redflashjesse@users.noreply.github.com>
2023-02-22 13:53:15 +0100

    Erster Versuch den Speicher netzdienlich zu laden und die Leistung Einspeisung auf 60% zu begrenzen mit Leistungsverlauf und Histogramm

--- main.py
+++ main.py
@@ -19,36 +19,60 @@ def main():
     Responsible for running everything sequentially, main has also the option to jump over some calculation and get
     some csv data, by setting values to false or true
     """
+    orginal_read = False
+    use_data_for_plot = False # or pickle
     speichergroessen = [12_000]
                             # list(range(500,  # start
                             #      10_000 + 1,  # end
                             #      500))  # step  # in Wh
     soc_start = None # input as float between 0.1,0,9; default = 0.45
+    # selection_days_start = list(range(4, # start
+      #                                365, # end
+       #                               20)) # step # in days for the year
+    startday = 150
+    endday = startday + 1
+    min_one_day = 1440
 
-    #print(f"--- Read in Smartmeter Daten ---")
-    #base_data = read_pv_netz_combined()
-    print(f"--- Read in Smartmeter Daten aus Pickle ---")
-    base_data = pd.read_pickle(f'documents/base_data.pkl')
-    #print(f"--- Pickle Daten ---")
-    #base_data.to_pickle(f'documents/base_data.pkl')
+    if orginal_read:
+        print(f"--- Read in Smartmeter Daten ---")
+        base_data = read_pv_netz_combined()
+        print(f"--- Pickle Daten ---")
+        base_data.to_pickle(f'documents/base_data.pkl')
 
-    print(len(base_data)) # hier fehlen wohl 2 erinträge
+    else:
+        print(f"--- Read in Smartmeter Daten aus Pickle ---")
+        base_data = pd.read_pickle(f'documents/base_data.pkl')
 
-    for size in speichergroessen:
-        print(f"--- Simulation Batterie nach Eigenverbrauch mit {size} Wh---")
-        batterypower_df = cal_battery_own_consumption(netz_pv=base_data,
-                                      soc_start=soc_start,
-                                     speichergroesse=size)
+    print(len(base_data))
 
-        print(f'--- Save Netz PV Speicher Eigenverbrauch als pickle ---')
-        batterypower_df.to_pickle(f'documents/netz_pv_mit_speichersimulation_eigenverbrauch.pkl')
+    if use_data_for_plot:
+        for size in speichergroessen:
+            print(f"--- Simulation Batterie nach Eigenverbrauch mit {size} Wh---")
+            batterypower_df = cal_battery_own_consumption(netz_pv=base_data,
+                                          soc_start=soc_start,
+                                         speichergroesse=size)
+
+            print(f'--- Save Netz PV Speicher Eigenverbrauch als pickle ---')
+            batterypower_df.to_pickle(f'documents/netz_pv_mit_speichersimulation_eigenverbrauch.pkl')
+
+            print(f"--- Simulation Batterie nach netzdienlich mit {size} Wh---")
+            batterypower_df = cal_battery_girdfriendly(netz_pv=batterypower_df,
+                                                          soc_start=soc_start,
+                                                          speichergroesse=size)
+
+            print(f'--- Save Netz PV Speicher netzdienlich als pickle ---')
+            batterypower_df.to_pickle(f'documents/netz_pv_mit_speichersimulation_netzdienlich.pkl')
+    else:
+        print(f'--- use pickle for data ---')
+        batterypower_df = pd.read_pickle(f'documents/netz_pv_mit_speichersimulation_netzdienlich.pkl')
 
         print(f'--- Plot: Leistungsverlauf ---')
         print(batterypower_df.keys())
-        #plot_power(df = batterypower_df, startday=15, endday=16, size=size)
-        plot_power_freq_dist(df=batterypower_df, startday=15, endday=16, size=size)
-
-
+        #for day in selection_days_start:
+         #   startday = day
+        for size in speichergroessen:
+            plot_power(df = batterypower_df, startday=startday, endday=endday, size=size, min_one_day = min_one_day)
+            plot_power_freq_dist(df=batterypower_df, startday=startday, endday=endday, size=size)
 
 
 
@@ -64,13 +88,12 @@ def selection_days():
          :return: list of filename
         """
     list_of_days = []
-    days_of_month = 2
+    list_of_days = list(range(1, 365, 15))
+    for day in list_of_days:
+        day
     numbers_of_month = list(1,
                             12)
-    for numbers_of_month in enumerate(days_of_month):
-        day = rd.randrange(1,30)
-        filename_pv = "data/PV-Anlage/Smartmeter_für_2021/"
-        filename_netz = "data/Netzanschluss/Smartdaten_für_2021/"
+
 
 
 
@@ -82,7 +105,6 @@ def read_modbus_pv(filename):
     :param filename: Name of the file that serves as input. Format : .csv
     :return: Dataframe
     """
-# TODO einlesen der Leistungswerte, die Umrechnung auf eine Wh ist noch nicht erfolgt, jeden Wert mit 1/60 multiplizieren
     df = pd.read_csv(f'{filename}', header=1, sep=';')  # read in
     df = df.iloc[1:]  # delete caption
     df = df.reset_index(drop=True)  # index starts with 0
@@ -109,7 +131,6 @@ def read_modbus_netz(filename):
       :param filename: Name of the file that serves as input. Format : .csv
       :return: Dataframe
       """
-    # TODO einlesen der Leistungswerte, die Umrechnung auf eine Wh ist noch nicht erfolgt, jeden Wert mit 1/60 multiplizieren
     df = pd.read_csv(f'{filename}',
                      header=1,
                      sep=';')  # read in
@@ -146,8 +167,8 @@ def readall_pv():
     # path2 = "data/PV-Anlage/April/Smart2/"
 
     # read all the files with extension .csv
-    filenames1 = glob.glob(path1 + "*4.csv")
-    filenames2 = glob.glob(path2 + "*4.csv")
+    filenames1 = glob.glob(path1 + "*.csv")
+    filenames2 = glob.glob(path2 + "*.csv")
     'data/PV-Anlage/Smart1/SN73144693-EM-PV-OW-2021-04-01.csv'
     # for loop to iterate each folder and concat to one df for a folder
 
@@ -198,8 +219,8 @@ def readall_netz():
     # todo Netz/smart1 Datei vom 2021-03-29 hat einen Fehler bei der formatierung des Unix Timestamp
 
     # read all the files with extension .csv
-    filenames1 = glob.glob(path1 + "*4.csv")
-    filenames2 = glob.glob(path2 + "*4.csv")
+    filenames1 = glob.glob(path1 + "*.csv")
+    filenames2 = glob.glob(path2 + "*.csv")
 
     # for loop to iterate each folder and concat to one df for a folder
     dfone = pd.DataFrame
@@ -238,7 +259,8 @@ def read_pv_netz_combined():
     # df = pd.concat([pv_df, netz_df], axis=1) # Todo
     df = pv_df.merge(netz_df, left_on='Timestamp', right_on='Timestamp', how='outer')
     df.fillna(0.0)
-    df = df.iloc[:-2:]
+    df = df.iloc[::]
+    # df = np.nan_to_num(df)
     return df
 
 def cal_pv_eigennutzung(netz_pv):
@@ -296,6 +318,7 @@ def cal_battery_own_consumption(netz_pv, speichergroesse, soc_start=None):
         netzbezug = [] # result amuont of power form the grid
         netzeinspeisung = []  # result amuont of power to the grid
         netzleistung = [] # grid power
+
         p_netzeinspeisung = 0
 
         if soc_start:
@@ -368,30 +391,236 @@ def cal_battery_own_consumption(netz_pv, speichergroesse, soc_start=None):
         return netz_pv # Leistungen_Speicher_eigenverbrauch
 
 
-def plot_power(df,startday, endday,size):
-    assert startday < endday
-    netzbezug = df[f'p_netzbezug_{size}Wh_eigenverbrauch'][startday*1440:endday*1440]
-    einspeisung = df[f'p_netzeinspeisung_{size}Wh_eigenverbrauch'][startday * 1440:endday * 1440]
-    einspeisung = einspeisung.mul(-1)
-    print(einspeisung)
-    plt.plot(netzbezug)
-    plt.plot(einspeisung)
-    plt.show()
+def cal_battery_girdfriendly(netz_pv, speichergroesse, soc_start=None):
+    """
+    Rechnung um den Speicher zu simulieren, die Leistungswerte und den neuen
+    State of Charge in einer Liste wiederzugeben. Je nach Varianten kann so eine mögliche Leistung aufgezeigt werden.
+    Parameter und Daten zu einem Speicher sind hier hinterlegt. Dieser kann durch die Speichergroesse angepasst werden.
+    Dies ist die Grundlage für eine Berechnung der möglichen Emissionen, bei gleicher Leistung.
+     :rtype: Dataframe
+     :param: Dataframe
+     :return: Dataframe
+     """
 
+    eta = 0.9  # Efficiency factor
+    soc_max = 0.9  # [range: 0-1 ]
+    soc_min = 0.1  # [range: 0-1 ]
+    zeit = 60  # [minute]
+
+    p_ges = speichergroesse / zeit  # [W] / [minute]
+    c_out = 1  # Coulombe Factor, depends on battery rating
+    c_in = 0.5  # Coulombe factor
+    min_flow_threshold = 0.1  # threshold for minimal flow for action to be taken [range: 0-1] in %
+
+    p_max_out = p_ges * c_out
+    p_min_out = p_max_out * min_flow_threshold
+    p_max_in = p_ges * c_in
+    p_min_in = p_max_in * min_flow_threshold
+
+    # Efficiency included in the borderline cases, set new limits
+    p_max_out *= eta
+    p_min_out *= eta
+    p_max_in = p_max_in * (1 + (1 - eta))
+    p_min_in = p_min_in * (1 + (1 - eta))
+
+    soc = []  # minute-wise soc
+    p_delta = []  # the difference in power per minute
+    soc_deltas = []  # the difference in state of charge
+    netzbezug = []  # result amuont of power form the grid
+    netzeinspeisung = []  # result amuont of power to the grid
+    netzleistung = []  # grid power
+
+    max_einspeise_p = 0.6 # entspricht dem prozentualen Anteil
+    p_netz_out_max = max(netz_pv['GridPowerOut']) * max_einspeise_p # value of max power input to the grid
+
+
+    if soc_start:
+        soc_akt = soc_start  # This represents an opportunity to specify a defined state of charge.
+    else:
+        soc_akt = soc_max / 2  # assumption: 45% charged at startup
+
+    for index, row in netz_pv.iterrows():
+        # Default: no change, no in, no out, p_soll = 0
+        soc_ist = soc_akt
+        soc_delta = 0
+        p_ist = 0
+        # Show network interface whether import or withdrawal takes place
+        p_soll = float(row['GridPowerIn']) - float(row['GridPowerOut'])
+
+        if p_soll > 0:  # check for positive
+            # p_supply = p_soll * (1 + (1 - eta))  # factor in losses
+            p_ist = min(p_max_out, p_soll)  # Threshold for upper bound
+
+            if p_ist >= p_min_out:  # Threshold for lower bound
+                soc_delta = ((p_ist * (1 + (1 - eta))) / (speichergroesse / 100)) / 100
+                soc_akt = soc_ist - soc_delta
+
+            # Capacity check, prevent depletion
+            if soc_akt < soc_min:
+                soc_akt = soc_ist
+                p_ist = 0
+                soc_delta = 0
+
+        if p_soll < 0:  # Query whether storage can be carried out with excess current # case p_soll negative
+            p_supply = abs(p_soll)
+            if p_supply > p_netz_out_max: # charing battery above a value depending by the power vaules of a day
+                p_charging_power = p_supply- p_netz_out_max
+                p_ist = min(p_max_in, p_charging_power)
+
+                if p_ist >= p_min_in:
+                    p_ist = -p_ist  # invert value to reflect incoming p
+                    soc_delta = ((p_ist * eta) / (speichergroesse / 100)) / 100
+                    soc_akt = soc_ist - soc_delta
+
+                # Capacity check, prevent overcharge
+                if soc_akt > soc_max:
+                    soc_akt = soc_ist
+                    p_ist = 0
+                    soc_delta = 0
+            else:
+                if p_supply < max_einspeise_p*0.1:
+                    p_ist = min(p_max_in, p_supply)
+
+                    if p_ist >= p_min_in:
+                        p_ist = -p_ist  # invert value to reflect incoming p
+                        soc_delta = ((p_ist * eta) / (speichergroesse / 100)) / 100
+                        soc_akt = soc_ist - soc_delta
+
+                    # Capacity check, prevent overcharge
+                    if soc_akt > soc_max:
+                        soc_akt = soc_ist
+                        p_ist = 0
+                        soc_delta = 0
+                else:
+                    soc_akt = soc_ist
+                    p_ist = 0
+                    soc_delta = 0
+
+
+            # calculation of the grid power by intergration of a battery
+        if p_ist > 0:
+            p_netzbezug = row['GridPowerIn'] - max(p_ist, 0)
+        if p_ist < 0:
+            p_netzeinspeisung = min(row['GridPowerOut'], p_netz_out_max) #row['GridPowerOut'] + min(p_ist, 0)
+        else:  # p_ist==0:
+            p_netzbezug = row['GridPowerIn']
+            p_netzeinspeisung = min(row['GridPowerOut'], p_netz_out_max) # row['GridPowerOut']
+
+        p_netz = p_netzbezug - p_netzeinspeisung
+
+        soc.append(soc_ist)
+        p_delta.append(p_ist)
+        soc_deltas.append(soc_delta)
+        netzbezug.append(p_netzbezug)
+        netzeinspeisung.append(p_netzeinspeisung)
+        netzleistung.append(p_netz)
+
+    netz_pv[f'p_delta_{speichergroesse}Wh_netzdienlich'] = p_delta
+    netz_pv[f'current_soc_{speichergroesse}Wh_netzdienlich'] = soc
+    netz_pv[f'soc_delta_{speichergroesse}Wh_netzdienlich'] = soc_deltas
+    netz_pv[f'p_netzbezug_{speichergroesse}Wh_netzdienlich'] = netzbezug
+    netz_pv[f'p_netzeinspeisung_{speichergroesse}Wh_netzdienlich'] = netzeinspeisung
+
+    return netz_pv  # Leistungen_Speicher_eigenverbrauch
+
+# starts of plots
+
+def plot_power(df, startday, endday, size, min_one_day):
+    assert startday < endday
+    date = df.index[startday*1440+ 125]
+    date = date.strftime('%Y-%m-%d')
+    print(date) #TODO
+    fig, ax = plt.subplots()
+
+    # Leistungsverlauf ohne Quartierspeicher
+    netzbezug_pure = df[f'GridPowerIn'][startday * 1440:endday * 1440]
+    einspeisung_pure = df[f'GridPowerOut'][startday * 1440:endday * 1440]
+    einspeisung_pure = einspeisung_pure.mul(-1)
+    plt.plot(netzbezug_pure, label='Netzbezug ohne Speicher',  alpha=0.4, zorder=1)
+    plt.plot(einspeisung_pure, label='Einspeisung ohne Speicher',  alpha=0.4, zorder=1)
+
+    # Leistungsverlauf mit Quatierspeicher nach eigenverbrauch
+    netzbezug_eigen = df[f'p_netzbezug_{size}Wh_eigenverbrauch'][startday * 1440:endday * 1440]
+    einspeisung_eigen = df[f'p_netzeinspeisung_{size}Wh_eigenverbrauch'][startday * 1440:endday * 1440]
+    einspeisung_eigen = einspeisung_eigen.mul(-1)
+    plt.plot(netzbezug_eigen, label='Netzbezug mit Speicher Eigenverbrauch',  alpha=0.4, zorder=2)
+    plt.plot(einspeisung_eigen, label='Netzbezug mit Speicher Eigenverbrauch',  alpha=0.4, zorder=2)
+
+    # Leistungsverlauf mit Quatierspeicher nach netzdienlich
+    netzbezug_eigen = df[f'p_netzbezug_{size}Wh_netzdienlich'][startday * 1440:endday * 1440]
+    einspeisung_eigen = df[f'p_netzeinspeisung_{size}Wh_netzdienlich'][startday * 1440:endday * 1440]
+    einspeisung_eigen = einspeisung_eigen.mul(-1)
+    plt.plot(netzbezug_eigen, label='Netzbezug mit Speicher netzdienlich',  alpha=0.4, zorder=3)
+    plt.plot(einspeisung_eigen, label='Netzbezug mit Speicher netzdienlich',  alpha=0.4, zorder=3)
+    plt.legend(loc='best')
+    ax.xaxis.set_ticks_position('bottom')  # the rest is the same
+    # plt.ylim(1000, )
+    ax.grid(True)
+    plt.xticks(rotation='vertical') #range(len(labels)), labels,
+    #plt.gcf().set_size_inches(15, 5)
+    plt.title('Leistungsverlauf')
+    # plt.suptitle('')
+    plt.xlabel('Zeit in h')
+    plt.ylabel('Leistung in Wh')
+    plt.tight_layout()
+    plt.show()
+    fig.savefig(f'graphs/Leistungsverlauf.png')
 
 def plot_power_freq_dist(df, startday, endday, size, binsize = 100):
     assert startday < endday
-    netzbezug = df[f'p_netzbezug_{size}Wh_eigenverbrauch'][startday * 1440:endday * 1440]
-    einspeisung = df[f'p_netzeinspeisung_{size}Wh_eigenverbrauch'][startday * 1440:endday * 1440]
-    min = einspeisung.max()
-    bins_out = math.ceil(min/binsize)
-    max = netzbezug.max()
-    bins_in = math.ceil(max/binsize)
+    bins= 25
+    # Betrachtung der Leistungen am Netzpunkt ohne Quartierspeicher
+    netzbezug_pure = df[f'GridPowerIn'][startday * 1440:endday * 1440]
+    einspeisung_pure = df[f'GridPowerOut'][startday * 1440:endday * 1440]
+    min_out = einspeisung_pure.max()
+    bins_out = math.ceil(min_out / binsize)
+    max_in = netzbezug_pure.max()
+    bins_in = math.ceil(max_in / binsize)
+    Leistung_pure = einspeisung_pure.mul(-1) + netzbezug_pure
 
-    print(min, max)
+    # Betrachtung der Leistungen am Netzpunkt mit Quartierspeicher nach Eigenverbrauchoptimiert
+    netzbezug_eigen = df[f'p_netzbezug_{size}Wh_eigenverbrauch'][startday * 1440:endday * 1440]
+    einspeisung_eigen = df[f'p_netzeinspeisung_{size}Wh_eigenverbrauch'][startday * 1440:endday * 1440]
+    min_out = einspeisung_eigen.max()
+    bins_out = math.ceil(min_out/binsize)
+    max_in = netzbezug_eigen.max()
+    bins_in = math.ceil(max_in/binsize)
+    Leistung_eigenverbrauch= einspeisung_eigen.mul(-1) + netzbezug_eigen
+
+    # Betrachtung der Leistungen am Netzpunkt mit Quartierspeicher nach Netzdienlichoptimiert
+    netzbezug_netz = df[f'p_netzbezug_{size}Wh_netzdienlich'][startday * 1440:endday * 1440]
+    einspeisung_netz = df[f'p_netzeinspeisung_{size}Wh_netzdienlich'][startday * 1440:endday * 1440]
+    min_out = einspeisung_netz.max()
+    bins_out = math.ceil(min_out/binsize)
+    max_in = netzbezug_netz.max()
+    bins_in = math.ceil(max_in/binsize)
+    Leistung_netz= einspeisung_netz.mul(-1) + netzbezug_netz
+
+    print(min_out, max_in)
     #print(einspeisung)
-    plt.hist(netzbezug, bins=bins_in)
-    plt.hist(einspeisung.mul(-1), bins= bins_out )
+    plt.style.use('fivethirtyeight')
+    print(type(round(min_out)), round(min_out))
+    bins_einspeisung = list(range(int(round(min_out)), 0, int(round(min_out)/10)))
+    bins_netzbezug = list(range(int(round(max_in)), 0, int(round(min_out)/10)))
+    #plt.hist(netzbezug, bins=bins_in, density= True, edgecolor='black')
+    plt.hist([Leistung_pure], bins=bins, # [bins_out, bins_in],
+             color=['blue' #, 'orange'
+                    ], density= True, edgecolor='black',
+             alpha=0.6, zorder=1) #, log=True, linewidth=1)
+    plt.hist([Leistung_eigenverbrauch], bins=bins, # [bins_out, bins_in],
+             color=['red'#, 'orange'
+                    ], density= True, edgecolor='black',
+             alpha=0.4, zorder=2) #, log=True, linewidth=1)
+    plt.hist([Leistung_netz], bins=bins, # [bins_out, bins_in],
+             color=['yellow'#, 'orange'
+                     ], density= True, edgecolor='black',
+             alpha=0.5, zorder=3) #, log=True, linewidth=1)
+    plt.axvline(0, color='red')
+    plt.legend()
+    plt.title('Histogramm')
+    plt.xlabel('Leistung in Wh')
+    plt.ylabel('Häufigkeit')
+    plt.tight_layout()
     plt.show()
 
 
